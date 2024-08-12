92b5c4381ce573c4c0e23e3453365130f193d169
Dominux <kir.gimranov@gmail.com>
2024-08-13 01:40:52 +0300

    Added some code that does not work

--- Makefile
+++ Makefile
@@ -1,5 +1,6 @@
 download:
 	wget -O ./lib/babylon.dynamicTerrain.min.js https://raw.githubusercontent.com/BabylonJS/Extensions/master/DynamicTerrain/dist/babylon.dynamicTerrain.min.js
+	wget -O ./lib/perlin.js https://raw.githubusercontent.com/josephg/noisejs/master/perlin.js
 
 run_server:
 	python3 -m http.server
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+import noise from './lib/perlin.js'
+
 const canvas = document.getElementById('canvas') // Get the canvas element
 const engine = new BABYLON.Engine(canvas, true) // Generate the BABYLON 3D engine
 
@@ -18,28 +20,77 @@ const createScene = function () {
   camera.setTarget(BABYLON.Vector3.Zero())
   // This attaches the camera to the canvas
   camera.attachControl(canvas, true)
-  // Creates a light, aiming 0,1,0 - to the sky
-  const light = new BABYLON.HemisphericLight(
-    'light',
-    new BABYLON.Vector3(0, 1, 0),
+
+  const light = new BABYLON.DirectionalLight(
+    'dirLight',
+    new BABYLON.Vector3(0.3, -0.42, -0.97),
     scene
   )
-  // Dim the light a small amount - 0 to 1
-  light.intensity = 0.7
-  // Built-in 'sphere' shape.
-  const sphere = BABYLON.MeshBuilder.CreateSphere(
-    'sphere',
-    { diameter: 2, segments: 32 },
-    scene
+  light.intensity = 1
+  light.autoUpdateExtends = false
+  light.shadowMinZ = 0.5
+  light.shadowMaxZ = 1.5
+  light.orthoLeft = -0.15
+  light.orthoRight = 0.2
+  light.orthoBottom = -0.15
+  light.orthoTop = 0.4
+
+  const defaultRSMTextureRatio = 8
+  const defaultGITextureRatio = 2
+
+  const outputDimensions = {
+    width: engine.getRenderWidth(true),
+    height: engine.getRenderHeight(true),
+  }
+
+  const rsmTextureDimensions = {
+    width: Math.floor(engine.getRenderWidth(true) / defaultRSMTextureRatio),
+    height: Math.floor(engine.getRenderHeight(true) / defaultRSMTextureRatio),
+  }
+
+  const giTextureDimensions = {
+    width: Math.floor(engine.getRenderWidth(true) / defaultGITextureRatio),
+    height: Math.floor(engine.getRenderHeight(true) / defaultGITextureRatio),
+  }
+
+  const giRSMs = [
+    new BABYLON.GIRSM(
+      new BABYLON.ReflectiveShadowMap(scene, light, rsmTextureDimensions)
+    ),
+  ]
+  giRSMs.forEach((girsm) => (girsm.rsm.forceUpdateLightParameters = true))
+
+  const giRSMMgr = new BABYLON.GIRSMManager(
+    scene,
+    outputDimensions,
+    giTextureDimensions,
+    2048
   )
-  // Move the sphere upward 1/2 its height
-  sphere.position.y = 1
-  // Built-in 'ground' shape.
-  const ground = BABYLON.MeshBuilder.CreateGround(
-    'ground',
-    { width: 6, height: 6 },
-    scene
-  )
+
+  giRSMMgr.addGIRSM(giRSMs)
+
+  giRSMMgr.enable = true
+  giRSMMgr.addMaterial() // no specific material transmitted to addMaterial, so all materials in the scene will be configured to render with GI
+
+  const mapSubX = 1000 // map number of points on the width
+  const mapSubZ = 800 // map number of points on the depth
+  const seed = 0.3 // set the noise seed for the Y value (elevation)
+  noise.setSeed(seed) // generate the simplex noise, don't care about this
+  const mapData = new Float32Array(mapSubX * mapSubZ * 3) // x3 because 3 values per point: x, y, z
+  for (let l = 0; l < mapSubZ; l++) {
+    // loop on depth points
+    for (let w = 0; w < mapSubX; w++) {
+      // loop on width points
+      const x = (w - mapSubX * 0.5) * 5.0 // distance inter-points = 5 on the width
+      const z = (l - mapSubZ * 0.5) * 2.0 // distance inter-points = 2 on the depth
+      const y = noise.simplex2(x, z) // elevation
+
+      mapData[3 * (l * mapSubX + w)] = x
+      mapData[3 * (l * mapSubX + w) + 1] = y
+      mapData[3 * (l * mapSubX + w) + 2] = z
+    }
+  }
+
   return scene
 }
 
